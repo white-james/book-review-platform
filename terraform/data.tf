9bf14d59d59fd735ba477d5f64f631d78e2d539b
jameswhite-bca <james.white1@bca.com>
2025-11-24 16:55:49 +0000

    refactor: remove unused Terraform files and clean up variable definitions

--- terraform/data.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_subscription" "this" {
-  subscription_id = var.azrm_subscription_id
-}
-
-data "azurerm_client_config" "current" {
-}
--- terraform/main.tf
+++
@@ -1,9 +0,0 @@
-#######             Application Resources              #######
-##############################################################
-
-# Generate a random suffix for unique naming
-resource "random_string" "unique_suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
