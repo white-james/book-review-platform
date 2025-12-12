80252a8527ddd6bb1b84ca72283743b4e0a441a7
jameswhite-bca <james.white1@bca.com>
2025-12-12 16:10:13 +0000

    feat: enable AKS cluster module with default node pool configuration

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -176,38 +176,38 @@ module "container_instance" {
 #   }
 # }
 
-# module "aks_cluster" {
-#   source = "git::https://github.com/Azure/terraform-azurerm-avm-res-containerservice-managedcluster.git?ref=v0.3.0"
+module "aks_cluster" {
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-containerservice-managedcluster.git?ref=v0.3.0"
 
-#   default_node_pool = {
-#     name                 = "default"
-#     vm_size              = "Standard_DS2_v2"
-#     node_count           = 3
-#     min_count            = 3
-#     max_count            = 3
-#     auto_scaling_enabled = true
-#     upgrade_settings = {
-#       max_surge = "10%"
-#     }
-#   }
-#   location            = var.azrm_resource_location
-#   name                = module.naming.kubernetes_cluster.name
-#   resource_group_name = module.resource_group.name
-#   azure_active_directory_role_based_access_control = {
-#     azure_rbac_enabled = true
-#     tenant_id          = data.azurerm_client_config.current.tenant_id
-#   }
-#   dns_prefix = "automaticexample"
-#   maintenance_window_auto_upgrade = {
-#     frequency   = "Weekly"
-#     interval    = "1"
-#     day_of_week = "Sunday"
-#     duration    = 4
-#     utc_offset  = "+00:00"
-#     start_time  = "00:00"
-#     start_date  = "2024-10-15T00:00:00Z"
-#   }
-#   managed_identities = {
-#     system_assigned = true
-#   }
-# }
+  default_node_pool = {
+    name                 = "default"
+    vm_size              = "Standard_DS2_v2"
+    node_count           = 3
+    min_count            = 3
+    max_count            = 3
+    auto_scaling_enabled = true
+    upgrade_settings = {
+      max_surge = "10%"
+    }
+  }
+  location            = var.azrm_resource_location
+  name                = module.naming.kubernetes_cluster.name
+  resource_group_name = module.resource_group.name
+  azure_active_directory_role_based_access_control = {
+    azure_rbac_enabled = true
+    tenant_id          = data.azurerm_client_config.current.tenant_id
+  }
+  dns_prefix = "automaticexample"
+  maintenance_window_auto_upgrade = {
+    frequency   = "Weekly"
+    interval    = "1"
+    day_of_week = "Sunday"
+    duration    = 4
+    utc_offset  = "+00:00"
+    start_time  = "00:00"
+    start_date  = "2024-10-15T00:00:00Z"
+  }
+  managed_identities = {
+    system_assigned = true
+  }
+}
