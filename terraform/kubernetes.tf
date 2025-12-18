1e1dd034791fb32c5ae77aa9bb95849b385d8fc9
jameswhite-bca <james.white1@bca.com>
2025-12-18 15:20:15 +0000

    refactor: update Kubernetes provider configuration to use AKS module outputs

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,21 +1,4 @@
 #############################################################
-#######            Kubernetes Provider                #######
-#############################################################
-
-# Get AKS credentials
-data "azurerm_kubernetes_cluster" "aks" {
-  name                = module.aks_cluster.name
-  resource_group_name = module.resource_group.name
-}
-
-provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-#############################################################
 #######         Kubernetes Resources                  #######
 #############################################################
 
