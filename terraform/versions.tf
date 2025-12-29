98afad35e99e91d61e3df65885fc3d7e6f17dfd4
jameswhite-bca <james.white1@bca.com>
2025-12-29 11:46:17 +0000

    chore: comment out Kubernetes provider configuration in versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -32,11 +32,11 @@ provider "azurerm" {
 provider "random" {
 }
 
-provider "kubernetes" {
-  host                   = module.aks_cluster.kube_admin_config[0].host
-  username               = module.aks_cluster.kube_admin_config[0].username  
-  password               = module.aks_cluster.kube_admin_config[0].password            
-  client_certificate     = base64decode(module.aks_cluster.kube_admin_config[0].client_certificate)
-  client_key             = base64decode(module.aks_cluster.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(module.aks_cluster.kube_admin_config[0].cluster_ca_certificate)
-}
+# provider "kubernetes" {
+#   host                   = module.aks_cluster.kube_admin_config[0].host
+#   username               = module.aks_cluster.kube_admin_config[0].username  
+#   password               = module.aks_cluster.kube_admin_config[0].password            
+#   client_certificate     = base64decode(module.aks_cluster.kube_admin_config[0].client_certificate)
+#   client_key             = base64decode(module.aks_cluster.kube_admin_config[0].client_key)
+#   cluster_ca_certificate = base64decode(module.aks_cluster.kube_admin_config[0].cluster_ca_certificate)
+# }
