75edf5cae9f1151c97d7b630451e74803e42a762
jameswhite-bca <james.white1@bca.com>
2025-12-31 10:53:19 +0000

    chore: remove deprecated Kubernetes resources and update container instance configuration

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -26,230 +26,119 @@ module "container_registry" {
   zone_redundancy_enabled = false
 }
 
-# Create a user-assigned managed identity for AKS control plane
-resource "azurerm_user_assigned_identity" "aks_control_plane" {
+module "container_instance" {
+  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-containerinstance-containergroup.git?ref=v0.2.0"
+
   location            = var.azrm_resource_location
-  name                = "${module.naming.kubernetes_cluster.name}-control-plane"
+  name                = module.naming.container_group.name
+  os_type             = "Linux"
   resource_group_name = module.resource_group.name
-}
+  restart_policy      = "Always"
 
-# Create a user-assigned managed identity for AKS kubelet
-resource "azurerm_user_assigned_identity" "aks_kubelet" {
-  location            = var.azrm_resource_location
-  name                = "${module.naming.kubernetes_cluster.name}-kubelet"
-  resource_group_name = module.resource_group.name
-}
-
-# Grant control plane identity "Managed Identity Operator" role on kubelet identity
-resource "azurerm_role_assignment" "aks_control_plane_mio" {
-  principal_id                     = azurerm_user_assigned_identity.aks_control_plane.principal_id
-  role_definition_name             = "Managed Identity Operator"
-  scope                            = azurerm_user_assigned_identity.aks_kubelet.id
-  skip_service_principal_aad_check = true
-}
-
-# Grant kubelet identity permission to pull images from ACR
-resource "azurerm_role_assignment" "aks_acr_pull" {
-  principal_id                     = azurerm_user_assigned_identity.aks_kubelet.principal_id
-  role_definition_name             = "AcrPull"
-  scope                            = module.container_registry.resource.id
-  skip_service_principal_aad_check = true
-}
-
-# module "container_instance" {
-#   source = "git::https://github.com/Azure/terraform-azurerm-avm-res-containerinstance-containergroup.git?ref=v0.2.0"
-
-#   location            = var.azrm_resource_location
-#   name                = module.naming.container_group.name
-#   os_type             = "Linux"
-#   resource_group_name = module.resource_group.name
-#   restart_policy      = "Always"
-
-#   # Image registry credentials for ACR
-#   image_registry_credential = {
-#     acr = {
-#       server   = module.container_registry.resource.login_server
-#       username = module.container_registry.resource.admin_username
-#       password = module.container_registry.resource.admin_password
-#     }
-#   }
-
-#   # Container definitions for Book Review Platform
-#   containers = {
-#     # PostgreSQL Database
-#     postgres = {
-#       name   = "postgres"
-#       image  = "${module.container_registry.resource.login_server}/postgres-custom:latest"
-#       cpu    = "1"
-#       memory = "2"
-#       ports = [
-#         {
-#           port     = 5432
-#           protocol = "TCP"
-#         }
-#       ]
-#       environment_variables = {
-#         "POSTGRES_DB"   = var.db_name
-#         "POSTGRES_USER" = var.db_user
-#         "PGDATA"        = "/var/lib/postgresql/data/pgdata"
-#       }
-#       secure_environment_variables = {
-#         "POSTGRES_PASSWORD" = var.db_password
-#       }
-#       volumes = {
-#         postgres-data = {
-#           name       = "postgres-data"
-#           mount_path = "/var/lib/postgresql/data"
-#           read_only  = false
-#           empty_dir  = true
-#         }
-#       }
-#     }
-
-#     # Node.js API Backend
-#     book-api = {
-#       name   = "book-api"
-#       image  = "${module.container_registry.resource.login_server}/book-api:1736221"
-#       cpu    = "1"
-#       memory = "1.5"
-#       ports = [
-#         {
-#           port     = 3000
-#           protocol = "TCP"
-#         }
-#       ]
-#       environment_variables = {
-#         "NODE_ENV" = "production"
-#         "PORT"     = "3000"
-#         "DB_HOST"  = "localhost"
-#         "DB_PORT"  = "5432"
-#         "DB_NAME"  = var.db_name
-#         "DB_USER"  = var.db_user
-#       }
-#       secure_environment_variables = {
-#         "DB_PASSWORD" = var.db_password
-#         "JWT_SECRET"  = var.jwt_secret
-#       }
-#       volumes = {}
-#     }
-
-#     # Nginx Frontend
-#     book-ui = {
-#       name   = "book-ui"
-#       image  = "${module.container_registry.resource.login_server}/book-ui:1736276"
-#       cpu    = "0.5"
-#       memory = "0.5"
-#       ports = [
-#         {
-#           port     = 80
-#           protocol = "TCP"
-#         }
-#       ]
-#       environment_variables        = {}
-#       secure_environment_variables = {}
-#       volumes                      = {}
-#     }
-#   }
-
-#   # Expose ports publicly
-#   exposed_ports = [
-#     {
-#       port     = 80
-#       protocol = "TCP"
-#     },
-#     {
-#       port     = 3000
-#       protocol = "TCP"
-#     }
-#   ]
-
-#   # Public DNS configuration (generates FQDN: <dns_name_label>.<region>.azurecontainer.io)
-#   dns_name_label = "${var.app_name}-${var.environment}-app"
-
-#   tags = {
-#     Environment = var.environment
-#     Application = "BookReviewPlatform"
-#     ManagedBy   = "Terraform"
-#   }
-# }
-
-# Storage Account - Commented out for ACI deployment
-# Azure Files (SMB) doesn't support the file permissions PostgreSQL requires
-# This will be uncommented for AKS where we'll use Azure Disk with Persistent Volume Claims
-# 
-# module "storage_account" {
-#   source = "git::https://github.com/Azure/terraform-azurerm-avm-res-storage-storageaccount.git?ref=v0.6.6"
-#
-#   location                 = var.azrm_resource_location
-#   name                     = module.naming.storage_account.name
-#   resource_group_name      = module.resource_group.name
-#   account_kind             = "StorageV2"
-#   account_replication_type = "LRS"
-#   account_tier             = "Standard"
-#   
-#   shared_access_key_enabled = true
-#   https_traffic_only_enabled = true
-#   min_tls_version = "TLS1_2"
-#   public_network_access_enabled = true
-#   
-#   network_rules = {
-#     bypass         = ["AzureServices"]
-#     default_action = "Allow"
-#     ip_rules       = []
-#     virtual_network_subnet_ids = []
-#   }
-#   
-#   tags = {
-#     Environment = var.environment
-#     Application = "BookReviewPlatform"
-#     ManagedBy   = "Terraform"
-#   }
-# }
-
-module "aks_cluster" {
-  source = "git::https://github.com/Azure/terraform-azurerm-avm-res-containerservice-managedcluster.git?ref=v0.3.0"
-
-  default_node_pool = {
-    name                 = "default"
-    vm_size              = "Standard_DS2_v2"
-    node_count           = 3
-    min_count            = 3
-    max_count            = 3
-    auto_scaling_enabled = true
-    upgrade_settings = {
-      max_surge = "10%"
+  # Image registry credentials for ACR
+  image_registry_credential = {
+    acr = {
+      server   = module.container_registry.resource.login_server
+      username = module.container_registry.resource.admin_username
+      password = module.container_registry.resource.admin_password
     }
   }
-  location            = var.azrm_resource_location
-  name                = module.naming.kubernetes_cluster.name
-  resource_group_name = module.resource_group.name
-  azure_active_directory_role_based_access_control = {
-    azure_rbac_enabled = true
-    tenant_id          = data.azurerm_client_config.current.tenant_id
-  }
-  local_account_disabled = false
-  dns_prefix = "automaticexample"
-  maintenance_window_auto_upgrade = {
-    frequency   = "Weekly"
-    interval    = "1"
-    day_of_week = "Sunday"
-    duration    = 4
-    utc_offset  = "+00:00"
-    start_time  = "00:00"
-    start_date  = "2024-10-15T00:00:00Z"
-  }
-  managed_identities = {
-    user_assigned_resource_ids = [
-      azurerm_user_assigned_identity.aks_control_plane.id
-    ]
-  }
-  kubelet_identity = {
-    client_id                 = azurerm_user_assigned_identity.aks_kubelet.client_id
-    object_id                 = azurerm_user_assigned_identity.aks_kubelet.principal_id
-    user_assigned_identity_id = azurerm_user_assigned_identity.aks_kubelet.id
+
+  # Container definitions for Book Review Platform
+  containers = {
+    # PostgreSQL Database
+    postgres = {
+      name   = "postgres"
+      image  = "${module.container_registry.resource.login_server}/postgres-custom:latest"
+      cpu    = "1"
+      memory = "2"
+      ports = [
+        {
+          port     = 5432
+          protocol = "TCP"
+        }
+      ]
+      environment_variables = {
+        "POSTGRES_DB"   = var.db_name
+        "POSTGRES_USER" = var.db_user
+        "PGDATA"        = "/var/lib/postgresql/data/pgdata"
+      }
+      secure_environment_variables = {
+        "POSTGRES_PASSWORD" = var.db_password
+      }
+      volumes = {
+        postgres-data = {
+          name       = "postgres-data"
+          mount_path = "/var/lib/postgresql/data"
+          read_only  = false
+          empty_dir  = true
+        }
+      }
+    }
+
+    # Node.js API Backend
+    book-api = {
+      name   = "book-api"
+      image  = "${module.container_registry.resource.login_server}/book-api:1736221"
+      cpu    = "1"
+      memory = "1.5"
+      ports = [
+        {
+          port     = 3000
+          protocol = "TCP"
+        }
+      ]
+      environment_variables = {
+        "NODE_ENV" = "production"
+        "PORT"     = "3000"
+        "DB_HOST"  = "localhost"
+        "DB_PORT"  = "5432"
+        "DB_NAME"  = var.db_name
+        "DB_USER"  = var.db_user
+      }
+      secure_environment_variables = {
+        "DB_PASSWORD" = var.db_password
+        "JWT_SECRET"  = var.jwt_secret
+      }
+      volumes = {}
+    }
+
+    # Nginx Frontend
+    book-ui = {
+      name   = "book-ui"
+      image  = "${module.container_registry.resource.login_server}/book-ui:1736276"
+      cpu    = "0.5"
+      memory = "0.5"
+      ports = [
+        {
+          port     = 80
+          protocol = "TCP"
+        }
+      ]
+      environment_variables        = {}
+      secure_environment_variables = {}
+      volumes                      = {}
+    }
   }
 
-  depends_on = [
-    azurerm_role_assignment.aks_control_plane_mio
+  # Expose ports publicly
+  exposed_ports = [
+    {
+      port     = 80
+      protocol = "TCP"
+    },
+    {
+      port     = 3000
+      protocol = "TCP"
+    }
   ]
+
+  # Public DNS configuration (generates FQDN: <dns_name_label>.<region>.azurecontainer.io)
+  dns_name_label = "${var.app_name}-${var.environment}-app"
+
+  tags = {
+    Environment = var.environment
+    Application = "BookReviewPlatform"
+    ManagedBy   = "Terraform"
+  }
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,21 +6,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.12.0, < 5.0.0"
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.6.0, < 4.0.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.35.0, < 3.0.0"
-    }
-
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.12.0, < 3.0.0"
-    }
   }
   backend "azurerm" {
   }
@@ -32,27 +17,4 @@ provider "azurerm" {
   }
   subscription_id = var.azrm_subscription_id
   tenant_id       = var.azrm_tenant_id
-}
-
-provider "random" {
-}
-
-provider "kubernetes" {
-  host                   = module.aks_cluster.kube_admin_config[0].host
-  username               = module.aks_cluster.kube_admin_config[0].username  
-  password               = module.aks_cluster.kube_admin_config[0].password            
-  client_certificate     = base64decode(module.aks_cluster.kube_admin_config[0].client_certificate)
-  client_key             = base64decode(module.aks_cluster.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(module.aks_cluster.kube_admin_config[0].cluster_ca_certificate)
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.aks_cluster.kube_admin_config[0].host
-    username               = module.aks_cluster.kube_admin_config[0].username
-    password               = module.aks_cluster.kube_admin_config[0].password
-    client_certificate     = base64decode(module.aks_cluster.kube_admin_config[0].client_certificate)
-    client_key             = base64decode(module.aks_cluster.kube_admin_config[0].client_key)
-    cluster_ca_certificate = base64decode(module.aks_cluster.kube_admin_config[0].cluster_ca_certificate)
-  }
 }
